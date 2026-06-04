0f8f4d2774419fb85fcc7ab5868e67f87dd3cbb7
Michelle's Web Design <michelleswebdesign@gmail.com>
2026-06-04 08:50:23 -0500

    Ref factored JavaScript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navMenu = document.querySelector('.nav-menu');
 
-menuToggle.addEventListener('click', function() {
-    navMenu.classList.toggle('open');
-});
-
+if (menuToggle && navMenu) {
+    menuToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('open');
+    });
+}
